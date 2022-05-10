7dd8d9eb4848ace30252afed5aab448269c0f730
Wolfero <ferorock44@gmail.com>
2022-05-10 10:26:38 +0100

    ✨Create Kata

---
+++ src/test/java/GildedRoseShould.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GildedRoseShould {
+
+    @Test
+    public void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("fixme", app.items[0].name);
+    }
+}
