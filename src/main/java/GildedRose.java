15ea63711ed967f59714ac01b45dc714bdf01e7e
Wolfero <ferorock44@gmail.com>
2022-05-10 12:09:51 +0100

    ♻ Refactor

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -56,7 +56,7 @@ class GildedRose {
                             }
                         }
                     } else {
-                        item.quality = item.quality - item.quality;
+                        item.quality = 0;
                     }
                 } else {
                     if (item.quality < 50) {
