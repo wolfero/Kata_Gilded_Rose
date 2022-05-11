5f3c80e37293da5d623e335901cbf5bc57ac1eef
Wolfero <ferorock44@gmail.com>
2022-05-11 13:26:22 +0100

    🔴 decreases by two point quality per day in conjured items

--- src/__tests__/GildedRose.test.ts
+++ src/__tests__/GildedRose.test.ts
@@ -4,7 +4,16 @@ import { GildedRose } from "../GildedRose";
 
 describe("Gilded Rose should", () => {
   it("decreases by one point quality per day in normal items", () => {
-    const normalItems: Item[] = [new Item("normal", 1, 1)];
+    const normalItems: Item[] = [new Item("normal_item", 1, 1)];
+    const app: GildedRose = new GildedRose(normalItems);
+
+    app.updateQuality();
+
+    expect(app.items[0].quality).toEqual(0);
+  });
+
+  it("decreases by two point quality per day in conjured items", () => {
+    const normalItems: Item[] = [new Item("conjured_item", 1, 2)];
     const app: GildedRose = new GildedRose(normalItems);
 
     app.updateQuality();
