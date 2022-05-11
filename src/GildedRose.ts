3249cee00dabb56fea82eddcaf3170a13b78b9cd
Wolfero <ferorock44@gmail.com>
2022-05-11 13:27:20 +0100

    🟢 decreases by two point quality per day in conjured items

--- src/GildedRose.ts
+++ src/GildedRose.ts
@@ -13,7 +13,11 @@ export class GildedRose {
         && this.items[i].name !== 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name !== 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
+            if (this.items[i].name.startsWith('conjured')) {
+              this.items[i].quality = this.items[i].quality - 2;
+            } else {
+              this.items[i].quality = this.items[i].quality - 1;
+            }
           }
         }
       } else {
