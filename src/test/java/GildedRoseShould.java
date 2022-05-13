db3357bc62d8ed0b58b2a6afd66d5c90dce0abf6
Wolfero <ferorock44@gmail.com>
2022-05-13 09:28:43 +0100

    ♻ Refactor name tests

--- src/test/java/GildedRoseShould.java
+++ src/test/java/GildedRoseShould.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GildedRoseShould {
 
     @Test
-    public void decreases_by_one_point_quality_per_day_in_normal_items() {
+    public void decreases_one_quality_point_per_day_in_normal_items() {
         Item normal_item = new Item("normal_item", 1, 1);
         Item[] items = new Item[]{normal_item};
         GildedRose app = new GildedRose(items);
@@ -16,7 +16,7 @@ public class GildedRoseShould {
     }
 
     @Test
-    public void decreases_by_two_point_quality_per_day_in_conjured_items() {
+    public void decreases_two_quality_points_per_day_in_conjured_items() {
         ConjuredItem conjured_item = new ConjuredItem("conjured_item", 1, 2);
         ConjuredItem[] conjured_items = new ConjuredItem[]{conjured_item};
         GildedRose app = new GildedRose(conjured_items);
